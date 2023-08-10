314591505c4aad896d0d86ca61051e4d17872481
Jeong-wonho <kallzzang@naver.com>
2023-08-10 22:20:57 +0900

    Adding dotenv Applying dotenv Applying the Index screen

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 
+const path = require('path');
+
+const dbUrl = process.env.DB_URL;
+const port = process.env.PORT;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded({extended:false}));
+
+app.get('/', (req,res,next) => {
+    res.send("<h1>hello world</h1>");
+});
+
+// listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
