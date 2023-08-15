f0f39bf9a79c9f1f2f65a533912b1b9eb41c8329
Jeong-wonho <kallzzang@naver.com>
2023-08-15 15:39:49 +0900

    crud 구현 완료

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -7,4 +7,8 @@ const router = express.Router();
 
 router.get('/', taskController.getTasks);
 
+router.post('/add-task', taskController.postTasks);
+router.post('/delete-task', taskController.deleteTask);
+router.post('/update-task', taskController.updateTask);
+
 module.exports = router;
