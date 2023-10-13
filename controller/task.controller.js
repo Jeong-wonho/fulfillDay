a0156155d8cb5864f20df073f2c8d47154f290ae
Jeong-wonho <kallzzang@naver.com>
2023-10-13 12:19:02 +0900

    rest api migration complete

--- controller/task.controller.js
+++ controller/task.controller.js
@@ -3,66 +3,89 @@ const Task = require("../models/task.model.js");
 /**
  * All Todo-list get
  */
-exports.getTasks = (req, res, next) => {
-  Task.find()
-    .then((tasks) => {
-      res.render("edit-task", {
-        tasks: tasks,
-        pageTitle: "Tasks",
-        path: "/"
-      });
-    })
-    .catch((err) => console.log(err));
+exports.getTasks = async (req, res, next) => {
+  try {
+    const todos = await Task.find().sort({ createdAt: -1 });
+
+    res.status(200).json({
+      message: "Fetched task succesfully",
+      todo: todos,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-/** 
+/**
  *  Todo-list post
  */
-exports.postTasks = (req, res, next) => {
-  const title = req.body.task;
+exports.postTasks = async (req, res, next) => {
+  const task = req.body.taskDesc;
   const tag = req.body.tag;
   //진행사항으로 변경이 필요하다.
   const completed = req.body.completed;
-  const task = new Task({
-    taskDesc: title,
+  const todo = new Task({
+    taskDesc: task,
     tag: tag,
     completed: completed
   });
-  task.save().then((result) => {
-    console.log('Created task');
-    res.redirect('/')
-  }).catch(err => console.log(err));
-}
-
-/** 
- * Todo-list delete
- */
-exports.deleteTask = (req, res, next) => {
-  const taskId = req.body.taskId;
-  console.log(taskId);
-  Task.findByIdAndRemove(taskId).then(() => {
-    console.log('Destroyed task');
-    res.redirect('/');
-  }).catch(err=>console.log(err));
-}
+  try {
+    await todo.save();
+    res.status(201).json({
+      message: "Post created succesfully!",
+      todo: todo,
+    })
+  }catch (err) {
+    console.log(err);
+  }
+};
 
 /**
  * Todo-list update
  */
-exports.updateTask = (req, res, next) => {
-  
+exports.updateTask = async (req, res, next) => {
   const taskId = req.body.taskId;
   const taskDesc = req.body.taskDesc;
+  const tag = req.body.tag;
+  const completed = req.body.completed;
 
-  console.log(taskId, taskDesc);
-
-  Task.findById(taskId).then((task) => {
+  try {
+    const task = await Task.findById(taskId)
+    if(!task) {
+      const error = new Error('Could not find post.');
+      error.statusCode = 404;
+      throw error;
+    }
     task.taskDesc = taskDesc;
-    return task.save();
-  }).then(result => {
-    console.log(result);
-    console.log("updated task!");
-    res.redirect('/');
-  }).catch(err => console.log(err))
+    task.tag = tag;
+    task.completed = completed;
+    const result = await task.save();
+    res.status(200).json({message: "Post updated", post: result});
 
-}
+  } catch (err) {
+    console.log(err);
+  }
+  
+};
+
+/**
+ * Todo-list delete
+ */
+exports.deleteTask = async (req, res, next) => {
+  const taskId = req.body.taskId;
+  try {
+    const task = await Task.findById(taskId);
+
+    if(!task) {
+      const error = new Error('Could not find post');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    await Task.findByIdAndRemove(taskId);
+
+    res.status(200).json({message: 'Deleted post.'})
+  } catch (err) {
+    console.log(err);
+  }
+};
